0801158d7dc1ee355f2aeffc7df266d57bd1148b
chelo67 <marceloacampora@gmail.com>
2025-01-05 21:35:51 +0000

    titulo en español y demas

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🎈 My nueva app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+cantidad = st.slider('selecciona la cantidad')
+
+st.write(f'la cantidad seleccionada es: {cantidad}')
+
+for i in range(cantidad):
+    st.button(f'{i}')
